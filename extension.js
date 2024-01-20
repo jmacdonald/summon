bc9d889ee68b52a4f7f309ab828510ab1ad622c6
Jordan MacDonald <jordan@wastedintelligence.com>
2024-01-19 23:22:43 -0500

    Initial commit

---
+++ extension.js
@@ -0,0 +1,9 @@
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+
+export default class SummonExtension extends Extension {
+    enable() {
+    }
+
+    disable() {
+    }
+}
