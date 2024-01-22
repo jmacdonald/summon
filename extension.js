cd340d41572ec1bb068d3cc36c466f760a29ffd9
Jordan MacDonald <jordan@wastedintelligence.com>
2024-01-21 23:15:43 -0500

    Initial basic app activation implementation

--- extension.js
+++ extension.js
@@ -7,21 +7,35 @@ const SCOPE = Meta.KeyBindingFlags.BUILTIN;
 
 export default class SummonExtension extends Extension {
     enable() {
-        this._display = Shell.Global.get().get_display();
-        this._settings = this.getSettings();
+        this.display = Shell.Global.get().get_display();
+        this.settings = this.getSettings();
+        this.appSystem = Shell.AppSystem.get_default();
 
-        log('setting up binding');
-        this._display.add_keybinding(
+        log('setting up key binding');
+        this.display.add_keybinding(
           'firefox-summon',
-          this._settings,
+          this.settings,
           SCOPE,
-          () => { log('firefox-summon keybind triggered') }
+          () => { this.activate_app('Firefox'); }
         );
     }
 
     disable() {
-        this._display.remove_keybinding('firefox-summon');
-        this._display = null;
-        this._settings = null;
+        this.display.remove_keybinding('firefox-summon');
+        this.display = null;
+        this.settings = null;
+    }
+
+    activate_app(name) {
+      const app = this.
+        appSystem.
+        get_running().
+        find(app => app.get_name() === name);
+
+      if (app === undefined) {
+          log(`Couldn't locate "${name}" app`);
+      } else {
+          app.activate();
+      }
     }
 }
