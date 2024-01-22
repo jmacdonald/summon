4822db205a29f96cfabf684960c113dc8f408a72
Jordan MacDonald <jordan@wastedintelligence.com>
2024-01-21 23:17:05 -0500

    Use two-space indentation

--- extension.js
+++ extension.js
@@ -6,36 +6,36 @@ import Shell from 'gi://Shell';
 const SCOPE = Meta.KeyBindingFlags.BUILTIN;
 
 export default class SummonExtension extends Extension {
-    enable() {
-        this.display = Shell.Global.get().get_display();
-        this.settings = this.getSettings();
-        this.appSystem = Shell.AppSystem.get_default();
+  enable() {
+    this.display = Shell.Global.get().get_display();
+    this.settings = this.getSettings();
+    this.appSystem = Shell.AppSystem.get_default();
 
-        log('setting up key binding');
-        this.display.add_keybinding(
-          'firefox-summon',
-          this.settings,
-          SCOPE,
-          () => { this.activate_app('Firefox'); }
-        );
+    log('setting up key binding');
+    this.display.add_keybinding(
+      'firefox-summon',
+      this.settings,
+      SCOPE,
+      () => { this.activate_app('Firefox'); }
+    );
+  }
+
+  disable() {
+    this.display.remove_keybinding('firefox-summon');
+    this.display = null;
+    this.settings = null;
+  }
+
+  activate_app(name) {
+    const app = this.
+      appSystem.
+      get_running().
+      find(app => app.get_name() === name);
+
+    if (app === undefined) {
+      log(`Couldn't locate "${name}" app`);
+    } else {
+      app.activate();
     }
-
-    disable() {
-        this.display.remove_keybinding('firefox-summon');
-        this.display = null;
-        this.settings = null;
-    }
-
-    activate_app(name) {
-      const app = this.
-        appSystem.
-        get_running().
-        find(app => app.get_name() === name);
-
-      if (app === undefined) {
-          log(`Couldn't locate "${name}" app`);
-      } else {
-          app.activate();
-      }
-    }
+  }
 }
